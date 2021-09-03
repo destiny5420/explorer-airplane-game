5982d7050da94869b9098e6e851c4a82cde94ef2
paperhsiao <destiny5420@gmail.com>
2021-09-04 06:29:28 +0800

    feat: init project

---
+++ src/js/main/App.js
@@ -0,0 +1,122 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+
+function App() {
+  console.log('The construct of App.')
+
+  /**
+   * Base
+   */
+  // Debug
+  const gui = new dat.GUI()
+
+  // Canvas
+  const canvas = document.querySelector('canvas.webgl')
+
+  // Scene
+  const scene = new THREE.Scene()
+
+  /**
+   * Lights
+   */
+  // Ambient light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.25)
+  gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+  scene.add(ambientLight)
+
+  // Directional light
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
+  directionalLight.castShadow = true
+  scene.add(directionalLight)
+
+  /**
+   * Materials
+   */
+  const material = new THREE.MeshStandardMaterial()
+  material.roughness = 0.7
+  gui.add(material, 'metalness').min(0).max(1).step(0.001)
+  gui.add(material, 'roughness').min(0).max(1).step(0.001)
+
+  /**
+   * Objects
+   */
+  const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
+  sphere.castShadow = true
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
+  plane.rotation.x = -Math.PI * 0.5
+  plane.position.y = -0.5
+  plane.receiveShadow = true
+
+  scene.add(sphere, plane)
+
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }
+
+  /**
+   * Camera
+   */
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+  camera.position.x = 1
+  camera.position.y = 1
+  camera.position.z = 2
+  scene.add(camera)
+
+  // Controls
+  const controls = new OrbitControls(camera, canvas)
+  controls.enableDamping = true
+
+  /**
+   * Renderer
+   */
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  })
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  /**
+   * Animate
+   */
+  const clock = new THREE.Clock()
+
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+  }
+
+  tick()
+
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+}
+
+export default App
